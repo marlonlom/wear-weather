65f9585c97c07286663e7d803f5adb24da522c23
Marlon López <malm.gm@gmail.com>
2023-10-23 22:14:39 -0500

    feat: added material extended icons dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,8 @@ dependencies {
   implementation("com.google.android.gms:play-services-location:21.0.1")
   implementation("com.squareup.retrofit2:retrofit:2.9.0")
   implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+  implementation("io.coil-kt:coil-compose:2.4.0")
+  implementation("androidx.compose.material:material-icons-extended:1.5.4")
   androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
   androidTestImplementation("androidx.compose.ui:ui-test-junit4")
   debugImplementation("androidx.compose.ui:ui-tooling")

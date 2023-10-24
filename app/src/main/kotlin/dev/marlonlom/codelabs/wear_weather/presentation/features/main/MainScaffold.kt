f1a5c33e32950424c9518015068b10d8c55bd280
Marlon López <malm.gm@gmail.com>
2023-10-23 22:27:35 -0500

    feat: updated ui for success getting weather data

--- app/src/main/kotlin/dev/marlonlom/codelabs/wear_weather/presentation/features/main/MainScaffold.kt
+++ app/src/main/kotlin/dev/marlonlom/codelabs/wear_weather/presentation/features/main/MainScaffold.kt
@@ -5,13 +5,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
 import androidx.wear.compose.foundation.lazy.ScalingLazyListState
 import androidx.wear.compose.foundation.lazy.rememberScalingLazyListState
+import androidx.wear.compose.material.CircularProgressIndicator
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.PositionIndicator
 import androidx.wear.compose.material.Scaffold
@@ -21,17 +25,28 @@ import androidx.wear.compose.material.Vignette
 import androidx.wear.compose.material.VignettePosition
 import dev.marlonlom.codelabs.wear_weather.presentation.features.location.AskingCurrentLocationScreen
 import dev.marlonlom.codelabs.wear_weather.presentation.features.location.UserLocationState
+import dev.marlonlom.codelabs.wear_weather.presentation.features.weather.openAppSettingsChipItem
+import dev.marlonlom.codelabs.wear_weather.presentation.features.weather.retryGetWeatherChipItem
+import dev.marlonlom.codelabs.wear_weather.presentation.features.weather.weatherConditionBox
+import dev.marlonlom.codelabs.wear_weather.presentation.features.weather.weatherDetailsBox
+import dev.marlonlom.codelabs.wear_weather.presentation.features.weather.weatherPlaceTextItem
 import dev.marlonlom.codelabs.wear_weather.presentation.network.WeatherApiUiState
 
 @Composable
 fun MainScaffold(
   userLocationState: UserLocationState,
   requestLocationPermissionAction: () -> Unit,
+  openAppSettingsAction: () -> Unit,
+  getCurrentWeatherAction: () -> Unit,
   weatherDataState: WeatherApiUiState
 ) {
   val scalingLazyListState: ScalingLazyListState = rememberScalingLazyListState()
   Scaffold(
-    timeText = { TimeText() },
+    timeText = {
+      if (!scalingLazyListState.isScrollInProgress) {
+        TimeText()
+      }
+    },
     vignette = { Vignette(VignettePosition.TopAndBottom) },
     positionIndicator = { PositionIndicator(scalingLazyListState) }
   ) {
@@ -45,6 +60,7 @@ fun MainScaffold(
       state = scalingLazyListState
     ) {
       Log.d("[MainScaffold]", "userLocationState=${userLocationState}")
+      Log.d("[MainScaffold]", "weatherDataState=${weatherDataState}")
       when (userLocationState) {
         UserLocationState.None -> {
           item {
@@ -59,17 +75,75 @@ fun MainScaffold(
 
         is UserLocationState.Failed -> {
           item {
-            Text(text = "Failed to get location")
+            Text(
+              modifier = Modifier
+                .fillMaxWidth()
+                .paddingFromBaseline(top = 40.dp, bottom = 20.dp)
+                .padding(horizontal = 10.dp),
+              text = "Failed to get location.\nCheck permissions for getting current location",
+              textAlign = TextAlign.Center,
+              style = MaterialTheme.typography.caption2
+            )
           }
+          openAppSettingsChipItem(openAppSettingsAction)
         }
 
         is UserLocationState.Located -> {
-          item {
-            Log.d("[MainScaffold]", "weatherDataState=$weatherDataState")
-            Text(text = "Success location: ${userLocationState.userLocation}")
+
+          when (weatherDataState) {
+            is WeatherApiUiState.Loading -> {
+              item {
+                CircularProgressIndicator()
+              }
+            }
+
+            is WeatherApiUiState.Failure -> {
+              item {
+                Text(
+                  modifier = Modifier
+                    .fillMaxWidth()
+                    .paddingFromBaseline(top = 40.dp, bottom = 20.dp)
+                    .padding(horizontal = 10.dp),
+                  text = "Failed to display current weather",
+                  textAlign = TextAlign.Center,
+                  style = MaterialTheme.typography.caption2,
+                )
+              }
+              retryGetWeatherChipItem(
+                openAppSettingsAction = { getCurrentWeatherAction() }
+              )
+              openAppSettingsChipItem(openAppSettingsAction)
+            }
+
+            is WeatherApiUiState.Success -> {
+              weatherPlaceTextItem(weatherDataState)
+              weatherConditionBox(weatherDataState)
+              weatherDetailsBox(weatherDataState)
+            }
+
+            WeatherApiUiState.Empty -> {
+              item {
+                Text(
+                  modifier = Modifier
+                    .fillMaxWidth()
+                    .paddingFromBaseline(top = 40.dp, bottom = 20.dp)
+                    .padding(horizontal = 10.dp),
+                  text = "No weather to display",
+                  textAlign = TextAlign.Center,
+                  style = MaterialTheme.typography.caption2,
+                )
+              }
+              retryGetWeatherChipItem(
+                openAppSettingsAction = { getCurrentWeatherAction() }
+              )
+              openAppSettingsChipItem(openAppSettingsAction)
+            }
+
           }
         }
       }
     }
   }
 }
+
+
